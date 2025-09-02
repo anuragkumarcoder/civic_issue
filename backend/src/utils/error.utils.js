1fafbd05b0357e89ff4deb68f653ce94f35bcc25
anuragkumarcoder <anuragkumar78702685001@gmail.com>
2025-09-03 01:08:02 +0530

    Add files via upload

---
+++ backend/src/utils/error.utils.js
@@ -0,0 +1,28 @@
+/**
+ * Custom error class for API errors
+ */
+class AppError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    this.isOperational = true;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+/**
+ * Catch async errors in controllers
+ * @param {Function} fn - Async function to wrap
+ */
+const catchAsync = (fn) => {
+  return (req, res, next) => {
+    fn(req, res, next).catch(next);
+  };
+};
+
+module.exports = {
+  AppError,
+  catchAsync,
+};
